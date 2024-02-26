f4256e822061726d137e71d3238d18ac515ca7e9
Fady Adal <2masadel@gmail.com>
2024-02-25 22:58:10 -0600

    add english as dest

--- coq_to_lean/src/coq_to_lean/languages.py
+++ coq_to_lean/src/coq_to_lean/languages.py
@@ -2,9 +2,11 @@ from typing import Optional, Type, TypedDict
 
 from pygments.lexer import Lexer
 from pygments.lexers.lean import Lean3Lexer
+from pygments.lexers.textfmts import NotmuchLexer
 from pygments.lexers.theorem import CoqLexer
 
 from coq_to_lean.destinations import Destination
+from coq_to_lean.destinations.english import English as EnglishDest
 from coq_to_lean.destinations.lean4 import Lean4 as Lean4Dest
 from coq_to_lean.sources import Source
 from coq_to_lean.sources.coq import Coq as CoqSrc
@@ -82,6 +84,18 @@ def next_weekday (d : Day) : Day :=
    | .sunday => .monday""",
             """\
 example : (next_weekday (next_weekday .saturday)) = .tuesday := sorry""",
+        ],
+    },
+    "english": {
+        "human_readable_name": "English",
+        "syntax_name": "english",
+        "dest_manager": EnglishDest,
+        "src_manager": None,
+        "lexer": NotmuchLexer,
+        "examples": [
+            "A day is either Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday.",
+            "To express the notion of a function 'next weekday' : day -> day, we say monday maps to tuesday, tuesday maps to wednesday, wednesday maps to thursday, thursday maps to friday, and all of friday saturday and sunday map to monday.",
+            "As an example, we can prove that the next weekday of the next weekday of saturday is tuesday.",
         ],
     },
 }
